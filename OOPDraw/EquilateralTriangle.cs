a11b51b7cc4de2d0f7294fb618b1ef5da2afd5b4
Stephen Gowen <sjgowen@gmail.com>
2020-02-18 15:54:28 +0000

    Q7

---
+++ OOPDraw/EquilateralTriangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDraw
+{
+    public class EquilateralTriangle
+    {
+        public float XOrigin { get; private set; }
+        public float YOrigin { get; private set; }
+        public float SideLength { get; private set; }
+
+        public EquilateralTriangle(float xOrigin, float yOrigin, float sideLength)
+        {
+            XOrigin = xOrigin;
+            YOrigin = yOrigin;
+            SideLength = sideLength;
+        }
+    }
+}
---
+++ OOPDraw/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDraw
+{
+    public class Rectangle
+    {
+        public float XOrigin { get; private set; }
+        public float YOrigin { get; private set; }
+        public float Width { get; private set; }
+        public float Height { get; private set; }
+
+        public Rectangle(float xOrigin, float yOrigin, float width, float height)
+        {
+            XOrigin = xOrigin;
+            YOrigin = yOrigin;
+            Width = width;
+            Height = height;
+        }
+    }
+}
